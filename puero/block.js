6a800452ecdd511916392eafcada55b64271aae8
antoniopuero <antonio.puero@gmail.com>
2012-11-02 01:29:24 +0200

    redo my task

--- puero/block.js
+++ puero/block.js
@@ -1,41 +1,40 @@
 $(document).ready(function () {
 	var layouts = $('.good_wrapper .good_layout'),
-		arrows = layouts.find('div[class$=_arrow]'),
-		current = 1,
-		blockWidth = parseInt(layouts.parent().css('width'), 10),
-		ulLength = $('.good ul li').length,
-		totalWidth = blockWidth * ulLength,
-		transition = function (container, loc, direction) {
-			var unit;
-			container.css('width', '1000px');
-			container.find('li').css('width', blockWidth-30);
-			if ( direction && loc !== 0 ) {
-				unit = ( direction === 'next' ) ? '-=' : '+=';
-			}
-			container.animate({
-				'margin-left': unit ? (unit + loc) : loc
-			});
-		};
+		arrows = layouts.find('div[class$=_arrow]');
 	layouts.hide();
-	layouts.parent().on('click', function () {
-		var descr = layouts.find('.good_description');
+	layouts.parent().hover(function () {
+		var descr = layouts.find('.good_description'),
+			block = $(this),
+			blockWidth = parseInt(block.css('width'), 10),
+			ul = block.find('ul'),
+			li = ul.find('li'),
+			ulLength = li.length,
+			current = 1;
 		layouts.fadeIn(400);
 		descr.animate({ top: '70%' }, 400);
-	});
-
-	arrows.on('click', function (e) {
-		var direction = $(this).data('dir'),
-			loc = blockWidth;
-
-		( direction === 'next' ) ? ++current : --current;
-		if ( current === 0 ) {
-			current = ulLength;
-			loc = totalWidth - blockWidth;
-			direction = 'next';
-		} else if ( current - 1 === ulLength ) {
-			current = 1;
-			loc = 0;
-		}
-		transition($('.good ul'), loc, direction);
+		arrows.click(function () {
+			var $this = $(this),
+				dir = $this.data('dir');
+			if ((dir === 'next')&&(current !== ulLength)) {
+				ul.css('width', '10000px');
+				li.css('width', blockWidth-30);
+				current += 1;
+				ul.animate({
+					'margin-left': '-=' + blockWidth
+				});
+			} else if (((dir === 'next')&&(current === ulLength))||((dir === 'prev')&&(current === 1))) {
+				console.log('return false');
+				return false;
+			} else {
+				ul.css('width', '10000px');
+				li.css('width', blockWidth-30);
+				current -= 1;
+				ul.animate({
+					'margin-left': '+=' + blockWidth
+				});
+			}
+		});
+	}, function () {
+		layouts.hide();
 	});
 });
