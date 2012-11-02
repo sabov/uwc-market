0aa86412860cc32df2b647b75ba95a0dda920632
antoniopuero <antonio.puero@gmail.com>
2012-11-02 12:56:33 +0200

    total redo

--- puero/block.js
+++ puero/block.js
@@ -12,17 +12,60 @@ var goods = (function () {
 
     //приватный метед который проходит по всем блокам и делает их четкими
     var initAllGoods = function (params) {
-        goods.forEach(function (elem, index) {
-            slider(elem, params);
+        console.log(goods);
+        $.each(goods, function (i, elem) {
+            slide(elem, params);
         });
     };
 
+    var setWidth = function(container, width) {
+        container.css('width', width);
+        return container;
+    }
+    var totalSetWidth = function(ul, li, params) {
+        setWidth(ul, params.ulWidth);
+        setWidth(li, params.blockWidth-30);
+    }
     //твой мего метод который принимает на вход ДОМ елемент(блок  продукта) и делает его четким
     //постарайяс вобще не использовать константы в коде. все константы задай в params при вызове init и сверху в блоке
     //var MY_CONST = 100500;
     var slide = function (good, params) {
         //тут реализовуешь всю свою логикку. Постарайся сделать методы slideToNext slideToPrev
         //и вызывать их при нажатии на стрелки.
+        var good = $(good);
+            layout = good.find('.good_layout'),
+            leftArrow = layout.find('.left'),
+            rightArrow = layout.find('.right'),
+            descr = layout.find('.good_description'),
+            blockWidth = parseInt(good.css('width'), 10),
+            ul = good.find('.good ul'),
+            li = ul.find('li'),
+            ulLength = li.length,
+            current = params.current;
+            console.log(blockWidth);
+        good.hover(function () {
+            layout.fadeIn(params.transitionTime);
+        }, function () {
+            layout.fadeOut(params.transitionTime);
+        });
+        leftArrow.on('click', function () {
+            totalSetWidth(ul, li, {ulWidth: params.width, blockWidth: blockWidth });
+            if(current === 1) {
+                return false;
+            } else {
+                current -= 1;
+                ul.animate({'margin-left': '+=' + blockWidth}, params.transitionTime);
+            }
+        });
+        rightArrow.on('click', function () {
+            totalSetWidth(ul, li, {ulWidth: params.width, blockWidth: blockWidth }, params.transitionTime);
+            if(current === ulLength) {
+                return false;
+            } else {
+                current += 1;
+                ul.animate({'margin-left': '-=' + blockWidth});
+            }
+        });
     };
 
     return {
@@ -36,64 +79,12 @@ var goods = (function () {
 
 //так должен вызываться твой код
 //пользователь слайдера должен просто передать уникальный класс блока c товаром и другие параметры
+$(document).ready(function(){
 goods.init({
-    goodClass: 'good',
-    width: 12312,
-    heights: 213123,
-    blabla: 'bla',
-    transitionTime: '',
-    andOther: 'andOther'
+    goodClass: '.good_wrapper',
+    current: 1,
+    width: '100000px',
+    transitionTime: 1000,
 });
 
-
-
-
-$(document).ready(function () {
-	var layouts = $('.good_wrapper .good_layout'),
-        //сделай сразу rightArrow and leftArrow
-		arrows = layouts.find('div[class$=_arrow]');
-    //Пусть layouts будет сразу скрыт! в css display none
-	layouts.hide();
-
-    //убрать ховер! добавь в css стиль :hover и там делай все что нужно
-	layouts.parent().hover(function () {
-		var descr = layouts.find('.good_description'),
-			block = $(this),
-        //к инту приводи так: +block.css('width') ! так красивее
-			blockWidth = parseInt(block.css('width'), 10),
-        //назови со смыслом переменные
-			ul = block.find('ul'),
-			li = ul.find('li'),
-			ulLength = li.length,
-            // констант не должно быть
-			current = 1;
-		layouts.fadeIn(400);
-		descr.animate({ top: '70%' }, 400);
-		arrows.click(function () {
-			var $this = $(this),
-				dir = $this.data('dir');
-			if ((dir === 'next')&&(current !== ulLength)) {
-                //неплохо бы в отделыный метод
-				ul.css('width', '10000px');
-				li.css('width', blockWidth-30);
-				current += 1;
-				ul.animate({
-					'margin-left': '-=' + blockWidth
-				});
-			} else if (((dir === 'next')&&(current === ulLength))||((dir === 'prev')&&(current === 1))) {
-				console.log('return false');
-				return false;
-			} else {
-                //неплохо бы в отделыный метод
-				ul.css('width', '10000px');
-				li.css('width', blockWidth-30);
-				current -= 1;
-				ul.animate({
-					'margin-left': '+=' + blockWidth
-				});
-			}
-		});
-	}, function () {
-		layouts.hide();
-	});
-});
+})
